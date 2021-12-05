f541320a425feef4ec90e19ca7616a017767df5d
Patreze Lopes <patreze.lopes@bycoders.com.br>
2021-12-05 01:16:09 -0300

    changed methods to get better async responses and cached responses

--- inventory/async_serializer.py
+++ inventory/async_serializer.py
@@ -2,49 +2,37 @@ from datetime import datetime
 from decimal import Decimal
 import concurrent.futures
 
+from django.db.models import F
+
 from inventory.models import Sale, Purchase, Product
 import asyncio
 
-from asgiref.sync import sync_to_async, async_to_sync
+from asgiref.sync import sync_to_async
+from inventory.utils import get_purchase as _get_purchase
+from inventory.utils import get_sale as _get_sale
+from inventory.utils import random_product as _random_product
 
-from inventory.utils import discount
 
-@sync_to_async
-def get_sale():
-    print(f'beginning sale- {datetime.now().time()}')
-    product = Product.objects.all().order_by('?')[0]
-    sales = Sale.objects.filter(product=product)
-    total_sales = 0
-    for sale in sales:
-        sale.price = sale.price * Decimal((1 - 0.10))
-        sale.save()
-        total_sales += sale.price
-    print(f'finished sale- {datetime.now().time()}')
-    return total_sales
+@sync_to_async(thread_sensitive=False)
+def async_get_sale(sales):
+    return _get_sale(sales)
+
+
+@sync_to_async(thread_sensitive=False)
+def async_get_purchase(purchases):
+    return _get_purchase(purchases)
 
 
 @sync_to_async
-def get_purchase():
-    print(f'beginning purchase- {datetime.now().time()}')
-    product = Product.objects.all().order_by('?')[0]
-    purchases = Purchase.objects.filter(product=product)
-    total_purchases = 0
-    for purchase in purchases:
-        if purchase.product.pk == 3:
-            purchase.price = 0
-            purchase.save()
-        if purchase.product.pk == 10:
-            purchase.price = purchase.prince * Decimal(1 - 0.10)
-            purchase.save()
-            total_purchases += purchase.price
-    print(f'finished purchase- {datetime.now().time()}')
-    return total_purchases
+def async_get_product():
+    return _random_product()
 
 
 async def async_discount():
-    sales = asyncio.create_task(get_sale())
-    purchases = asyncio.create_task(get_purchase())
-
-    sales, purchases = await asyncio.gather(sales, purchases)
-    return sales, purchases
+    product = await async_get_product()
+    task_purchases = sync_to_async(Purchase.objects.filter, thread_sensitive=False)(product=product)
+    task_sales = sync_to_async(Sale.objects.filter, thread_sensitive=False)(product=product)
+    sales, purchases = await asyncio.gather(task_sales, task_purchases)
+    total_sales, total_purchases = await asyncio.gather(async_get_sale(sales), async_get_purchase(purchases))
+    return total_sales, total_purchases
 
--- inventory/urls.py
+++ inventory/urls.py
@@ -8,11 +8,13 @@ router.register(r'purchase', views.PurchaseViewSet, basename='purchase')
 router.register(r'sale', views.SaleViewSet, basename='sale')
 sync_view = SyncAsync.as_view({"get": "sync_get"})
 async_view = SyncAsync.as_view({"get": "async_get"})
+cached_view = SyncAsync.as_view({"get": "cached_view"})
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('sync_view', sync_view, name='sync_view'),
-    path('async_view', async_view, name='async_view'),
+    path('sync', sync_view, name='sync_view'),
+    path('async', async_view, name='async_view'),
+    path('cached', cached_view, name='cached_view'),
 ]
